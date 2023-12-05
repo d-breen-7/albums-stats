e9cf56758b4d27d348d024a3db5bae52d6fa5cb0
xdbx7 <d13reen7@gmail.com>
2023-12-05 20:02:19 +0000

    chart updates

--- scripts/all_albums.js
+++ scripts/all_albums.js
@@ -1,41 +1,63 @@
-var ca_margins = { top: 20, right: 55, bottom: 25, left: 10 };
+var ca_margins = { top: 20, right: 55, bottom: 30, left: 10 },
+  ca_width = d3.select("#stats-image-1").node().offsetWidth,
+  ca_height = d3.select("#stats-image-1").node().offsetHeight,
+  current_year = new Date().getFullYear(),
+  parse_date = d3.timeParse("%Y-%m-%d");
 
 d3.json(
   "https://i3aounsm6zgjctztzbplywogfy0gnuij.lambda-url.eu-west-1.on.aws/albums"
+  // "data//albums.json"
 ).then(function (response) {
-  var data = tidy(
+  var res_data = tidy(
     response.data,
     mutate({
-      day: (d) => d3.timeParse("%Y-%m-%d")(d.date).getDate(),
-      month: (d) => d3.timeParse("%Y-%m-%d")(d.date).getMonth(),
+      year: (d) => parse_date(d.date).getFullYear(),
+      day: (d) => parse_date(d.date).getDate(),
+      month: (d) => parse_date(d.date).getMonth(),
+      norm_date: (d) =>
+        new Date(
+          current_year,
+          parse_date(d.date).getMonth(),
+          parse_date(d.date).getDate()
+        ),
     })
   );
 
-  var unique_albums = Number(response.unique).toLocaleString(),
+  // TODO: update logic backend
+  res_data[0].day_count_all = 0;
+
+  var data = group_by_year(res_data),
     total_albums = Number(d3.max(data, (d) => d.total_all)).toLocaleString();
 
-  d3.select("#stats-1-text")
-    .append("h2")
-    .html(
-      "I've listened to " +
-        unique_albums +
-        " unique albums, " +
-        total_albums +
-        " when including relistens"
-    );
+  // Define SVG
+  var ca_svg = d3
+    .select("#stats-image-1")
+    .append("svg")
+    .attr("id", "ca-svg")
+    .attr("viewBox", [0, 0, ca_width, ca_height]);
 
-  var ca_width = d3.select("#stats-image-1").node().offsetWidth,
-    ca_height = d3.select("#stats-image-1").node().offsetHeight;
+  // Define tooltip
+  var tooltip = d3
+    .select("#stats-image-1")
+    .append("div")
+    .attr("class", "ca-tooltip")
+    .style("visibility", "hidden");
 
-  // Add X axis
+  // X axis
   var ca_x = d3
     .scaleTime()
-    .domain(d3.extent(data, (d) => d3.timeParse("%Y-%m-%d")(d.date)))
+    .domain(d3.extent(data, (d) => parse_date(d.date)))
     .range([ca_margins.left, ca_width - ca_margins.right]);
 
   var ca_x_axis = d3.axisBottom(ca_x).ticks(d3.timeYear);
 
-  // Add Y axis
+  ca_svg
+    .append("g")
+    .attr("class", "ca-x-axis")
+    .attr("transform", "translate(0," + (ca_height - ca_margins.bottom) + ")")
+    .call(ca_x_axis);
+
+  // Y axis
   var ca_y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => +d.total_all + 5)])
@@ -44,88 +66,432 @@ d3.json(
   var ca_y_axis = d3
     .axisRight()
     .scale(ca_y)
-    .ticks(d3.max(data, (d) => +d.total_all + 50) / 1000)
+    .ticks(d3.max(data, (d) => +d.total_all) / 1000)
     .tickSize(ca_width - ca_margins.left - ca_margins.right)
     .tickFormat((d) => Number(d).toLocaleString());
 
-  var ca_svg = d3
-    .select("#stats-image-1")
-    .append("svg")
-    .attr("id", "ca-svg")
-    .attr("viewBox", [0, 0, ca_width, ca_height]);
-
-  ca_svg
-    .append("g")
-    .attr("class", "ca-x-axis")
-    .attr("transform", "translate(0," + (ca_height - ca_margins.bottom) + ")")
-    .call(ca_x_axis);
-
-  // Add y axis labels
   ca_svg
     .append("g")
     .attr("class", "ca-y-axis")
     .attr("transform", "translate(" + ca_margins.left + ", 0)")
     .call(ca_y_axis);
 
-  // Data on 1st of each year
-  first_year = tidy(
-    data,
-    filter((d) => d.day === 1 && d.month === 0)
-  );
-
-  // Year lines
+  // Year grid line
   ca_svg
     .selectAll("ca-svg")
-    .data(first_year)
+    .data(data.filter((d) => d.day === 1 && d.month === 0))
     .enter()
     .append("line")
     .attr("class", "ca-year-lines")
-    .attr("x1", (d) => ca_x(d3.timeParse("%Y-%m-%d")(d.date)))
-    .attr("x2", (d) => ca_x(d3.timeParse("%Y-%m-%d")(d.date)))
+    .attr("id", "ca-year-lines")
+    .attr("x1", (d) => ca_x(parse_date(d.date)))
+    .attr("x2", (d) => ca_x(parse_date(d.date)))
     .attr("y1", ca_y(0))
     .attr("y2", ca_y(d3.max(data, (d) => d.total_all)));
 
-  ca_svg
-    .append("text")
-    .attr("class", "ca-today-text")
-    .attr("x", ca_x(d3.max(data, (d) => d3.timeParse("%Y-%m-%d")(d.date))))
-    .attr("y", ca_y(d3.max(data, (d) => d.total_all)) - 7.5)
-    .text(Number(d3.max(data, (d) => d.total_all)).toLocaleString())
-    .attr("alignment-baseline", "middle");
-
-  // // Info for starting at 116
-  // ca_svg
-  //   .append("text")
-  //   .attr("class", "ca-info")
-  //   .attr("x", ca_x(d3.min(data, (d) => d3.timeParse("%Y-%m-%d")(d.date))) + 5)
-  //   .attr("y", ca_y(d3.min(data, (d) => d.total_all) + 150))
-  //   .text(d3.min(data, (d) => d.total_all) + " albums listened to pre-2019")
-  //   .attr("alignment-baseline", "middle");
-
-  // define the area
+  // Albums area
   var ca_area = d3
     .area()
-    .x((d) => ca_x(d3.timeParse("%Y-%m-%d")(d.date)))
-    .y0((d) => ca_y(0))
+    .x((d) => ca_x(parse_date(d.date)))
+    .y0(ca_y(0))
     .y1((d) => ca_y(d.total_all));
 
-  // define the line
-  var ca_line = d3
-    .line()
-    .x((d) => ca_x(d3.timeParse("%Y-%m-%d")(d.date)))
-    .y((d) => ca_y(d.total_all));
-
-  // fill area under curve
   ca_svg
     .append("path")
     .data([data])
     .attr("class", "ca-area")
     .attr("d", ca_area);
 
-  // add the line.
+  // Albums line
+  var ca_line = d3
+    .line()
+    .x((d) => ca_x(parse_date(d.date)))
+    .y((d) => ca_y(d.total_all));
+
   ca_svg
     .append("path")
     .data([data])
     .attr("class", "ca-line-all")
     .attr("d", ca_line);
+
+  // Add heading sub text
+  d3.select("#stats-1-text")
+    .append("h2")
+    .attr("id", "stats-1-sub-text")
+    .html(
+      "I've listened to <span style='color: #1db954; font-weight: 1000';>" +
+        total_albums +
+        "</span> albums (including re-listens) since I started tracking in 2019"
+    );
+
+  // Text for total
+  ca_svg
+    .append("text")
+    .attr("class", "ca-today-text")
+    .attr("id", "ca-today-text")
+    .attr("x", ca_x(d3.max(data, (d) => parse_date(d.date))))
+    .attr("y", ca_y(d3.max(data, (d) => d.total_all)) - 7.5)
+    .text(Number(d3.max(data, (d) => d.total_all)).toLocaleString())
+    .attr("alignment-baseline", "middle");
+
+  function transition_period(period) {
+    // Remove existing elements
+    d3.select("#stats-1-sub-text").remove();
+    d3.select("#ca-today-text").remove();
+    d3.selectAll("#ca-year-lines").remove();
+    d3.selectAll(".ca-line-all").remove();
+    d3.selectAll(".ca-line-current").remove();
+    d3.selectAll(".ca-line-old").remove();
+    d3.selectAll(".ca-area").remove();
+    d3.selectAll("#clip").remove();
+
+    if (period !== "ytd") {
+      // Logic for specific years/all time
+      var period_data =
+          period === "all-time" ? data : data.filter((d) => d.year == period),
+        period_total =
+          period === "all-time"
+            ? d3.max(period_data, (d) => +d.total_all)
+            : d3.max(period_data, (d) => +d.cum_sum);
+
+      // Update X axis
+      var ca_x = d3
+        .scaleTime()
+        .domain(d3.extent(period_data, (d) => parse_date(d.date)))
+        .range([ca_margins.left, ca_width - ca_margins.right]);
+
+      var ca_x_axis =
+        period === "all-time"
+          ? d3.axisBottom(ca_x).ticks(d3.timeYear)
+          : d3
+              .axisBottom(ca_x)
+              .ticks(d3.timeMonth)
+              .tickFormat(d3.timeFormat("%b"));
+
+      ca_svg
+        .select(".ca-x-axis")
+        .transition()
+        .duration(1000)
+        .attr(
+          "transform",
+          "translate(0," + (ca_height - ca_margins.bottom) + ")"
+        )
+        .call(ca_x_axis);
+
+      // Update Y axis
+      var ca_y = d3
+        .scaleLinear()
+        .domain([0, period_total + 5])
+        .range([ca_height - ca_margins.bottom, ca_margins.top]);
+
+      var ca_y_axis = d3
+        .axisRight()
+        .scale(ca_y)
+        .ticks(
+          period_total < 10
+            ? period_total / 1
+            : period_total < 50
+            ? period_total / 5
+            : period_total < 100
+            ? period_total / 10
+            : period_total < 200
+            ? period_total / 25
+            : period_total < 600
+            ? period_total / 100
+            : period_total < 1000
+            ? period_total / 200
+            : period_total < 2500
+            ? period_total / 250
+            : period_total / 1000
+        )
+        .tickSize(ca_width - ca_margins.left - ca_margins.right)
+        .tickFormat((d) => Number(d).toLocaleString());
+
+      ca_svg
+        .select(".ca-y-axis")
+        .transition()
+        .duration(1000)
+        .attr("transform", "translate(" + ca_margins.left + ", 0)")
+        .call(ca_y_axis);
+
+      // Update year grid lines
+      ca_svg
+        .selectAll("ca-svg")
+        .data(
+          period === "all-time"
+            ? data.filter((d) => d.day === 1 && d.month === 0)
+            : period_data.filter((d) => d.day === 1)
+        )
+        .enter()
+        .append("line")
+        .attr("class", "ca-year-lines")
+        .attr("id", "ca-year-lines")
+        .attr("x1", (d) => ca_x(parse_date(d.date)))
+        .attr("x2", (d) => ca_x(parse_date(d.date)))
+        .attr("y1", ca_y(period_total))
+        .attr("y2", ca_y(0));
+
+      // Update Area
+      var ca_area =
+        period == "all-time"
+          ? d3
+              .area()
+              .x((d) => ca_x(parse_date(d.date)))
+              .y0(ca_y(0))
+              .y1((d) => ca_y(d.total_all))
+          : d3
+              .area()
+              .x((d) => ca_x(parse_date(d.date)))
+              .y0(ca_y(0))
+              .y1((d) => ca_y(d.cum_sum));
+
+      // Define a clipPath
+      ca_svg
+        .append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", 0)
+        .attr("height", "100%");
+
+      ca_svg
+        .append("path")
+        .attr("class", "ca-area")
+        .data([period_data])
+        .attr("d", ca_area)
+        .attr("clip-path", "url(#clip)");
+
+      ca_svg
+        .select("#clip rect")
+        .transition()
+        .duration(1000)
+        .ease(d3.easeLinear)
+        .attr("width", ca_width);
+
+      // Update Line
+      var ca_line = d3
+        .line()
+        .x((d) => ca_x(parse_date(d.date)))
+        .y((d) => ca_y(period === "all-time" ? d.total_all : d.cum_sum));
+
+      var new_line = ca_svg
+        .append("path")
+        .data([period_data])
+        .attr("class", "ca-line-all")
+        .attr("d", ca_line);
+
+      total_length = new_line.node().getTotalLength();
+
+      new_line
+        .attr("stroke-dasharray", total_length + " " + total_length)
+        .attr("stroke-dashoffset", total_length)
+        .transition()
+        .duration(1000)
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0);
+
+      // Update heading sub text
+      var summary_text =
+        period == "all-time"
+          ? "I've listened to <span style='color: #1db954; font-weight: 1000';>" +
+            Number(period_total).toLocaleString() +
+            "</span> albums since I started tracking in 2019"
+          : period == current_year
+          ? "So far this year, I've listened to <span style='color: #1db954; font-weight: 1000';>" +
+            Number(period_total).toLocaleString() +
+            "</span> albums"
+          : "In " +
+            period +
+            ", I listened to <span style='color: #1db954; font-weight: 1000';>" +
+            Number(period_total).toLocaleString() +
+            "</span> albums";
+
+      d3.select("#stats-1-text")
+        .append("h2")
+        .attr("id", "stats-1-sub-text")
+        .html(summary_text);
+
+      // Update total text
+      ca_svg
+        .append("text")
+        .attr("class", "ca-today-text")
+        .attr("id", "ca-today-text")
+        .attr("x", ca_x(d3.max(period_data, (d) => parse_date(d.date))))
+        .attr("y", ca_y(period_total) - 7.5)
+        .text("")
+        .transition()
+        .delay(1000)
+        .text(Number(period_total).toLocaleString())
+        .attr("alignment-baseline", "middle");
+    } else {
+      // Logic for when ytd selected
+      var ytd = d3.max(
+        data.filter((d) => d.year == current_year),
+        (d) => d.norm_date
+      );
+
+      ytd_data = data.filter((d) => d.norm_date <= ytd);
+
+      // Update X axis
+      var ca_x = d3
+        .scaleTime()
+        .domain(
+          d3.extent(
+            ytd_data.filter((d) => d.year == current_year),
+            (d) => d.norm_date
+          )
+        )
+        .range([ca_margins.left, ca_width - ca_margins.right]);
+
+      var ca_x_axis = d3
+        .axisBottom(ca_x)
+        .ticks(d3.timeMonth)
+        .tickFormat(d3.timeFormat("%b"));
+
+      ca_svg
+        .select(".ca-x-axis")
+        .transition()
+        .duration(1000)
+        .attr(
+          "transform",
+          "translate(0," + (ca_height - ca_margins.bottom) + ")"
+        )
+        .call(ca_x_axis);
+
+      // Update Y axis
+      var ca_y = d3
+        .scaleLinear()
+        .domain([0, d3.max(ytd_data, (d) => +d.cum_sum) + 5])
+        .range([ca_height - ca_margins.bottom, ca_margins.top]);
+
+      period_total = d3.max(ytd_data, (d) => +d.cum_sum);
+
+      var ca_y_axis = d3
+        .axisRight()
+        .scale(ca_y)
+        .ticks(
+          period_total < 10
+            ? period_total / 1
+            : period_total < 50
+            ? period_total / 5
+            : period_total < 100
+            ? period_total / 10
+            : period_total < 200
+            ? period_total / 25
+            : period_total < 600
+            ? period_total / 100
+            : period_total < 1000
+            ? period_total / 200
+            : period_total < 2500
+            ? period_total / 250
+            : period_total / 1000
+        )
+        .tickSize(ca_width - ca_margins.left - ca_margins.right)
+        .tickFormat((d) => Number(d).toLocaleString());
+
+      ca_svg
+        .select(".ca-y-axis")
+        .transition()
+        .duration(1000)
+        .attr("transform", "translate(" + ca_margins.left + ", 0)")
+        .call(ca_y_axis);
+
+      // Loop through each year of data
+      d3.set(ytd_data.map((d) => d.year))
+        .values()
+        .forEach((year, i) => {
+          // Draw line
+          var ca_line = d3
+            .line()
+            .x((d) => ca_x(d.norm_date))
+            .y((d) => ca_y(d.cum_sum));
+
+          var new_line = ca_svg
+            .append("path")
+            .data([ytd_data.filter((d) => d.year == year)])
+            .attr("class", "ca-line-all")
+            .attr("d", ca_line)
+            .on("mouseover", function (d) {
+              return tooltip.style("visibility", "visible").text(year);
+            })
+            .on("mousemove", function () {
+              return tooltip
+                .style("top", d3.event.pageY - 25 + "px")
+                .style("left", d3.event.pageX - 20 + "px");
+            })
+            .on("mouseout", function () {
+              return tooltip.style("visibility", "hidden");
+            });
+
+          total_length = new_line.node().getTotalLength();
+
+          new_line
+            .attr("stroke-dasharray", total_length + " " + total_length)
+            .attr("stroke-dashoffset", total_length)
+            .transition()
+            .delay(i * 1100)
+            .duration(1000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+            .transition()
+            .duration(0)
+            .attr(
+              "class",
+              year == current_year ? "ca-line-current" : "ca-line-old"
+            );
+        });
+
+      // Update year grid lines
+      ca_svg
+        .selectAll("ca-svg")
+        .data(ytd_data.filter((d) => d.day === 1 && d.year == current_year))
+        .enter()
+        .append("line")
+        .attr("class", "ca-year-lines")
+        .attr("id", "ca-year-lines")
+        .attr("x1", (d) => ca_x(d.norm_date))
+        .attr("x2", (d) => ca_x(d.norm_date))
+        .attr("y1", "100%")
+        .attr("y2", "0%");
+
+      ytd_total = Number(
+        d3.max(
+          ytd_data.filter((d) => d.year == current_year),
+          (d) => d.cum_sum
+        )
+      ).toLocaleString();
+
+      rank = tidy(
+        ytd_data,
+        filter(
+          (d) =>
+            d.norm_date.getFullYear() === ytd.getFullYear() &&
+            d.norm_date.getMonth() === ytd.getMonth() &&
+            d.norm_date.getDate() === ytd.getDate()
+        ),
+        arrange(desc("cum_sum")),
+        mutate({ ytd_rank: (_, i) => i + 1 }),
+        filter((d) => d.year === current_year)
+      )[0]["ytd_rank"];
+
+      var summary_text =
+        "The <span style='color: #1db954; font-weight: 1000';>" +
+        ytd_total +
+        "</span> albums so far this year ranks no. <span style='color: #1db954; font-weight: 1000';>" +
+        rank +
+        "</span> compared to previous years";
+
+      // Update heading sub text
+      d3.select("#stats-1-text")
+        .append("h2")
+        .attr("id", "stats-1-sub-text")
+        .html(summary_text);
+    }
+  }
+
+  // Update based on option selected
+  d3.selectAll('input[name="albums-year"]').on("change", function () {
+    if (this.checked) {
+      transition_period(this.value);
+    }
+  });
 });
--- scripts/functions.js
+++ scripts/functions.js
@@ -21,3 +21,30 @@ const {
   mutateWithSummary,
   rowNumber,
 } = Tidy;
+
+function group_by_year(data) {
+  var groupedData = {};
+
+  // Group data by year
+  data.forEach((row) => {
+    const year = row.year;
+    if (!groupedData[year]) {
+      groupedData[year] = [];
+    }
+    groupedData[year].push(row);
+  });
+
+  var result = [];
+
+  // Calculate the cumulative sum for year
+  for (const year in groupedData) {
+    let cumulativeSum = 0;
+    groupedData[year].forEach((row) => {
+      cumulativeSum += row.day_count_all;
+      const newRow = { ...row, cum_sum: cumulativeSum };
+      result.push(newRow);
+    });
+  }
+
+  return result;
+}
