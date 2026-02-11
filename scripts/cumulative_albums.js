07f2a13c73b92870c4b9bb088e2a455c92e0d943
d-breen-7 <d13reen7@gmail.com>
2026-02-11 15:44:25 +0000

    misc

--- scripts/cumulative_albums.js
+++ scripts/cumulative_albums.js
@@ -27,7 +27,7 @@ d3.json(
   An overview of my album listens since the start of 2019. During 2019, I mostly listened
   to new releases before starting to consciously listen to more albums in 2020. 
   After excluding relistens, which are included below, I have listened to 
-  <span style='color: #1db954; font-weight: 1000'>~${unique_albums}</span> unique albums.
+  <span style='color: #1db954; font-weight: 1000'>${unique_albums}</span> unique albums.
   The barcode shows days with no album listens.
   <br><span style='color: #a9a9a9'>2019 starts at 116 to account for some albums where I 
   don't have the exact listen date.</span>`;
